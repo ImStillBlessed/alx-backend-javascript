49ebf56a12f48fd959665cdb89e6f466cbfd2f58
Oigbochie Blessed <eprince1800@gmail.com>
2024-05-04 13:17:58 +0100

    eslint formatting

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -4,9 +4,11 @@ export default class HolbertonCourse {
     this._length = length;
     this._students = students;
   }
+
   get name() {
     return this._name;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw TypeError('Name must be a string');
@@ -17,15 +19,18 @@ export default class HolbertonCourse {
   get length() {
     return this._length;
   }
+
   set length(length) {
     if (typeof length !== 'number') {
       throw TypeError('Length must be a number');
     }
     this._length = length;
   }
+
   get students() {
     return this._students;
   }
+
   set students(students) {
     if (!Array.isArray(students)) {
       throw TypeError('Students must be an array');
--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -5,27 +5,33 @@ export default class Pricing {
     this._amount = amount;
     this._currency = currency;
   }
+
   get amount() {
     return this._amount;
   }
+
   set amount(amount) {
     if (typeof amount !== 'number') {
       throw TypeError('Amount must be a number');
     }
     this._amount = amount;
   }
+
   get currency() {
     return this._currency;
   }
+
   set currency(currency) {
     if (!(currency instanceof Currency)) {
       throw TypeError('Currency must be a Currency');
     }
     this._currency = currency;
   }
+
   displayFullPrice() {
     return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
+
   static convertPrice(amount, conversionRate) {
     return amount * conversionRate;
   }
