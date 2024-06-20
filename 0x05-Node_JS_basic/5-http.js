a547ca21abb8cbf0c3c01f1ba33e0c7761ee068c
Oigbochie Blessed <eprince1800@gmail.com>
2024-06-20 19:56:27 +0100

    corrections to task 3 and 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,43 +1,39 @@
 const http = require('http');
 const fs = require('fs');
-const path = require('path');
 
-const countStudents = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+const countStudents = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const studentGroups = {};
+    let totalStudents = 0;
+
+    for (let i = 1; i < lines.length; i += 1) {
+      const [firstname, , , field] = lines[i]
+        .split(',')
+        .map((value) => value.trim());
+      if (!studentGroups[field]) {
+        studentGroups[field] = [];
       }
+      studentGroups[field].push(firstname);
+      totalStudents += 1;
+    }
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const studentGroups = {};
-      let totalStudents = 0;
-
-      for (let i = 1; i < lines.length; i++) {
-        const [firstname, , , field] = lines[i]
-          .split(',')
-          .map((value) => value.trim());
-        if (!studentGroups[field]) {
-          studentGroups[field] = [];
-        }
-        studentGroups[field].push(firstname);
-        totalStudents++;
-      }
-
-      let result = `Number of students: ${totalStudents}\n`;
-      for (const [field, students] of Object.entries(studentGroups)) {
-        result += `Number of students in ${field}: ${
-          students.length
+    let result = `Number of students: ${totalStudents}\n`;
+    for (const [field, students] of Object.entries(studentGroups)) {
+      result += `Number of students in ${field}: ${students.length
         }. List: ${students.join(', ')}\n`;
-      }
-      resolve(result.trim());
-    });
+    }
+    resolve(result.trim());
   });
-};
+});
 
 const app = http.createServer((req, res) => {
-  const url = req.url;
+  const { url } = req.url;
   if (url === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
