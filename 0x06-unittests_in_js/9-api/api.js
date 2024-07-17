9d41111f176ebc7a06a1a06f747f5866ba7a9711
Oigbochie Blessed <eprince1800@gmail.com>
2024-07-17 17:55:36 +0100

    task 9

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  if (isNaN(req.params.id)) {
+    res.status(404).end();
+  } else {
+    const id = parseInt(req.params.id);
+    res.send(`Payment methods for cart ${id}`);
+  }
+});
+
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
