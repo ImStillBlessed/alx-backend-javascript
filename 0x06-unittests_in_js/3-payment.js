e6257cd08d7bb37edaa92e9a4961764953238c8c
Oigbochie Blessed <eprince1800@gmail.com>
2024-07-28 16:15:25 +0100

    changed the output

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
   const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total cost is: ${totalCost}`);
+  console.log(`The total is: ${totalCost}`);
 };
 
 module.exports = sendPaymentRequestToApi;
